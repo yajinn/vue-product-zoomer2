0faacc1318bfd580a8564b6a43fbf532030b4d11
yasin.coskun <yajinn@gmail.com>
2021-09-21 13:51:02 +0300

    webpack edit

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,63 +1,63 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const isProduction = process.env.NODE_ENV !== 'production'
-const webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const isProduction = process.env.NODE_ENV !== "production";
+const webpack = require("webpack");
 
 module.exports = {
-  mode: isProduction ? 'production' : 'development',
+  mode: isProduction ? "production" : "development",
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000
   },
   devServer: {
-    contentBase: './develoment',
+    contentBase: "./develoment",
     hot: true
   },
   devtool: false,
   entry: {
-    vendor: ['vue'],
-    app: './src/dev.js'
+    vendor: ["vue"],
+    app: "./src/dev.js"
   },
   output: {
-    path: path.resolve(__dirname, 'develoment'),
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js'
+    path: path.resolve(__dirname, "develoment"),
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js"
   },
   resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.json', '.jsx', '.vue'],
+    modules: ["node_modules"],
+    extensions: [".js", ".json", ".jsx", ".vue"],
     plugins: []
   },
   module: {
     rules: [
       {
-          test: /\.(png|jpg|gif|svg)$/,
-          use: [
-            {
-              loader: 'file-loader'
-            },
-          ],
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
         options: {
-          postcss: [require('precss')(), require('autoprefixer')()]
+          postcss: [require("precss")(), require("autoprefixer")()]
         }
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: file => /node_modules/.test(file) && !/\.vue\.js/.test(file),
         options: {
-          presets: ['@babel/preset-env'],
+          presets: ["@babel/preset-env"],
           plugins: [
             [
-              '@babel/plugin-proposal-class-properties',
+              "@babel/plugin-proposal-class-properties",
               {
                 loose: true
               }
@@ -68,37 +68,35 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'vue-style-loader',
+          "vue-style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: { importLoaders: 1 }
           }
         ]
       },
       {
         test: /\.less$/,
-        use: ['vue-style-loader', 'css-loader', 'less-loader']
+        use: ["vue-style-loader", "css-loader", "less-loader"]
       },
       {
         test: /\.scss$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+        use: ["vue-style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/assets/index.html',
-      title: 'Vue Product Zoomer',
-      appMountId: 'app'
+      template: "./src/assets/index.html",
+      title: "Vue Product Zoom",
+      appMountId: "app"
     }),
     new webpack.HotModuleReplacementPlugin(),
     new CleanWebpackPlugin(),
     new UglifyJsPlugin({
       uglifyOptions: {
-        compress: {
-          warnings: false
-        }
+        warnings: false
       },
       sourceMap: isProduction ? false : true,
       parallel: true
@@ -106,13 +104,13 @@ module.exports = {
   ],
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       minSize: 30000,
       maxSize: 0,
       minChunks: 1,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
+      automaticNameDelimiter: "~",
       name: true,
       cacheGroups: {
         vendors: {
@@ -128,7 +126,7 @@ module.exports = {
     },
     runtimeChunk: {
       // manage common package relationship
-      name: 'manifest'
+      name: "manifest"
     }
   }
-}
+};
--- webpack.publish.config.js
+++ webpack.publish.config.js
@@ -1,48 +1,48 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const isProduction = process.env.NODE_ENV !== 'production'
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const isProduction = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: isProduction ? 'production' : 'development',
+  mode: isProduction ? "production" : "development",
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist"
   },
-  devtool: isProduction ? 'source-map' : false,
+  devtool: isProduction ? "source-map" : false,
   entry: {
-    app: './src/publish.js'
+    app: "./src/publish.js"
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'assets/js/[name].bundle.js',
-    library: 'ProductZoomer',
-    libraryTarget: 'umd'
+    path: path.resolve(__dirname, "dist"),
+    filename: "assets/js/[name].bundle.js",
+    library: "ProductZoomer",
+    libraryTarget: "umd"
   },
   resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.json', '.jsx', '.vue'],
+    modules: ["node_modules"],
+    extensions: [".js", ".json", ".jsx", ".vue"],
     plugins: []
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
         options: {
-          postcss: [require('precss')(), require('autoprefixer')()]
+          postcss: [require("precss")(), require("autoprefixer")()]
         }
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: file => /node_modules/.test(file) && !/\.vue\.js/.test(file),
         options: {
-          presets: ['@babel/preset-env'],
+          presets: ["@babel/preset-env"],
           plugins: [
             [
-              '@babel/plugin-proposal-class-properties',
+              "@babel/plugin-proposal-class-properties",
               {
                 loose: true
               }
@@ -53,47 +53,45 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'vue-style-loader',
+          "vue-style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: { importLoaders: 1 }
           }
         ]
       },
       {
         test: /\.less$/,
-        use: ['vue-style-loader', 'css-loader', 'less-loader']
+        use: ["vue-style-loader", "css-loader", "less-loader"]
       },
       {
         test: /\.scss$/,
-        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+        use: ["vue-style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              limit: 8192,
-            },
+              limit: 8192
+            }
           }
-        ],
-      },
+        ]
+      }
     ]
   },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/assets/index.html',
-      title: 'Vue Product Zoomer',
-      appMountId: 'app'
+      template: "./src/assets/index.html",
+      title: "Vue Product Zoom",
+      appMountId: "app"
     }),
     new CleanWebpackPlugin(),
     new UglifyJsPlugin({
       uglifyOptions: {
-        compress: {
-          warnings: false
-        }
+        warnings: false
       },
       sourceMap: isProduction ? false : true,
       parallel: true
@@ -101,13 +99,13 @@ module.exports = {
   ],
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       minSize: 30000,
       maxSize: 0,
       minChunks: 1,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
-      automaticNameDelimiter: '~',
+      automaticNameDelimiter: "~",
       name: true,
       cacheGroups: {
         vendors: {
@@ -122,4 +120,4 @@ module.exports = {
       }
     }
   }
-}
+};
